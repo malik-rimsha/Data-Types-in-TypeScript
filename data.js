28dc49a8480ef836599dbac8b48593d3a0dc7e98
DUA-UMER <duam95623@gmail.com>
2024-05-08 02:35:51 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,37 @@
+// // // number| string | boolean  | any
+// let age:number = 10
+// // // string
+// let name1:string = "rimsha"
+// // // boolean
+// let isstudent: boolean = true;
+// // // any
+// let flexiblevalue: any;
+// flexiblevalue = 10;
+// console.log(flexiblevalue);
+// import { Console, count } from "console";
+// flexiblevalue = "Malik Rimsha";
+// console.log(flexiblevalue);
+// flexiblevalue = true;
+// console.log(flexiblevalue);
+// flexiblevalue =[1,2,3]
+// console.log(flexiblevalue);
+// undefined | unknown | BigInt | symbol | null
+// undefined
+let x = undefined;
+console.log(undefined);
+// unknown
+let userinput;
+userinput = 10;
+userinput = "ten";
+console.log(userinput);
+// // bigInt
+let bignumber = 10000n;
+console.log(bignumber);
+// // symbol
+let key1 = Symbol("key");
+let key2 = Symbol("key");
+console.log(key1 == key2); //false
+// // null
+let resuslt = null;
+console.log(resuslt);
+export {};
